8f43edbea6df3172d8abd84a3b3aec066319027f
michaeldeistler <michael.deistler95@gmail.com>
2024-07-16 15:10:54 +0200

    change setup.py to pyproject.toml

--- tridiax/__init__.py
+++ tridiax/__init__.py
@@ -1,3 +1,5 @@
+from tridiax.__version__ import __version__  # noqa: F401
+
 from tridiax.thomas import thomas_solve
 from tridiax.divide_and_conquer import divide_conquer_solve, divide_conquer_index
 from tridiax.stone import stone_solve
---
+++ tridiax/__version__.py
@@ -0,0 +1,6 @@
+# This file is part of tridiax, a toolkit for solving tridiagonal systems. tridiax is 
+# licensed under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
+
+VERSION = (0, 1, 0)
+
+__version__ = ".".join(map(str, VERSION))
