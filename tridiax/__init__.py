931a0c797f40b6a9bf205c2823b201c9a29848d2
michaeldeistler <michael.deistler95@gmail.com>
2024-07-16 15:18:25 +0200

    add text for license

--- tridiax/__init__.py
+++ tridiax/__init__.py
@@ -1,3 +1,6 @@
+# This file is part of tridiax, a toolkit for solving tridiagonal systems. tridiax is
+# licensed under the Apache License Version 2.0, see <https://www.apache.org/licenses/>
+
 from tridiax.__version__ import __version__  # noqa: F401
 
 from tridiax.thomas import thomas_solve
